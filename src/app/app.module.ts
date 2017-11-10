7b1685ed0e5b22f049e105380eda5333e6bc91ca
TheoRev <hrevillafernandez79@gmail.com>
2017-11-10 00:42:59 -0500

    Firs comit y creacion UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
+import { AppComponent } from "./app.component";
 
-import { AppComponent } from './app.component';
+// Material Angular
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "./material.module";
+import "hammerjs";
 
+import { QuestionDetailComponent } from "./question/question-detail.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, QuestionDetailComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
