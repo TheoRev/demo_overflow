39ca6f59f30c6bbd551fce90326a3a6f09f9544f
TheoRev <hrevillafernandez79@gmail.com>
2017-11-12 00:28:54 -0500

    Completando la Ui de questions y answers Creación de UI de Signin

--- src/app/answer/answer-form.component.ts
+++ src/app/answer/answer-form.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Question } from "./../question/question.model";
+import { Answer } from "./answer.model";
+import { User } from "../auth/user.model";
+import { Component, Input } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-    selector: 'app-answer-form',
-    templateUrl: './answer-form.component.html'
+  selector: "app-answer-form",
+  templateUrl: "./answer-form.component.html",
+  styles: [
+    `
+    form{
+      display: flex;
+      flex-direction: column;
+      width: 90%
+      margin: 40px auto;
+      margin-top: 20px;
+      margin-bottom: 40px;
+    }
+    `
+  ]
 })
 export class AnswerFormComponent {
-    onSubmit(form: NgForm) {
-        console.log(form.value.description);
-    }
+  @Input() question: Question;
+  onSubmit(form: NgForm) {
+    const answer = new Answer(
+      form.value.description,
+      this.question,
+      new Date(),
+      new User(null, null, "Almendra", "Cárdenas Mesa")
+    );
+    this.question.answers.unshift(answer);
+    form.reset();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,31 @@ import { AppComponent } from "./app.component";
 // Material Angular
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialModule } from "./material.module";
-import "hammerjs";
+import 'hammerjs';
 
 import { QuestionDetailComponent } from "./question/question-detail.component";
+import { SigninScreenComponent } from "./auth/signin-screen.component";
 
-import { MomentModule } from 'angular2-moment';
+import { MomentModule } from "angular2-moment";
 import { AnswerFormComponent } from "./answer/answer-form.component";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, QuestionDetailComponent
-    , AnswerFormComponent
+  declarations: [
+    AppComponent,
+    QuestionDetailComponent,
+    AnswerFormComponent,
+    SigninScreenComponent
   ],
-  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule, MomentModule, FormsModule
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    MomentModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
